bdc038ea6713bf49b2a984ce9a4bc562b1144f93
Nick Decroos <ndcroos@users.noreply.github.com>
2018-11-11 16:33:23 +0100

    Create RemainderClass.rs

---
+++ src/RemainderClass.rs
@@ -0,0 +1,40 @@
+pub struct RemainderClassRing {
+
+
+}
+
+impl RemainderClasRing {
+  fn units() -> {
+  
+  
+  }
+  
+  
+  fn order() -> {
+  
+  
+  }
+  
+  fn subgroups() -> {
+  
+  
+  }
+  
+  fn generators() -> {
+  
+  
+  }
+
+}
+
+
+
+fn caleyTable() -> {
+
+}
+
+
+fn test {
+  r : RemainderClassRing
+  order : int32 = 
+}

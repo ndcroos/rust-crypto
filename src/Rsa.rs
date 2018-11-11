c7587b9923c4a5ae80771cad5f1fdeff8857ca9c
Nick Decroos <ndcroos@users.noreply.github.com>
2018-11-11 16:58:55 +0100

    Update and rename rsa.rs to Rsa.rs

---
+++ src/Rsa.rs
@@ -0,0 +1,40 @@
+
+fn main() {
+  let int n = 12345;
+  let int eA = 5;
+  let int eB = 13;
+  let int c = 54321;
+  
+  decrypt(n, eA, eB, c);
+  
+  
+}
+
+
+fn decrypt(n : i32, eA : i32, eB : i32, c : i32){
+  println!("Prime factors");
+  
+  let p = FindPrimeFactor(n);
+  let q = n / p;
+  let phi = (p - 1) * (q - 1);
+  
+  println!("p = " + p");
+  println!("q = " + q");
+  println!("phi = " + phi");
+    
+  println!("Bezout");
+    
+  println!("Calculate m");
+}
+    
+    
+fn remainder() -> i32 {
+    
+    
+}
+    
+fn bezoutInverse() -> i32 {
+    
+    
+    
+}
